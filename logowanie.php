fdd9c9186829fa4f6d84e19f1496adb715f9ff30
Ihuarraquax <zablo432432@o2.pl>
2018-06-06 13:23:44 +0200

    zmiana index.html na php i dodanie nietestowanego logowania

--- logowanie.php
+++ logowanie.php
@@ -1,43 +1,44 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blogdb";
+// sprawdzenie czy dane zostaly wyslane postem
+if ((!isset($_POST['login'])) || (!isset($_POST['haslo']))) {
+    header('Location: index.php');
+    exit();
+}
+
+
+require_once "connect.php";
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = @new mysqli($host, $db_user, $db_password, $db_name);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else
-    $sql = "SELECT login, haslo FROM users";
-$result = $conn->query($sql);
+} else {
+    //ochrona przed wstrzykiwaniem sqla
+    $login = $_POST['login'];
+    $haslo = $_POST['haslo'];
 
+    $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+    $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
 
+    if ($result = @$conn->query(
+                    sprintf("SELECT * FROM users WHERE login='%s' AND haslo='%s'", mysqli_real_escape_string($polaczenie, $login), mysqli_real_escape_string($polaczenie, $haslo)))) {
+
+        $ilu_userow = $rezultat->num_rows;
+        if ($ilu_userow > 0) {
+            session_start();
+            $_SESSION['zalogowany'] = true;
+        } else {
+            $_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+            header('Location: index.php');
+        }
+    }
+}
 
 $conn->close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   echo $_POST["login"];
-   echo $_POST["haslo"];
-
-
 
 
 
